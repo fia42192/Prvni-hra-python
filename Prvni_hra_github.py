393a3cb85ba9393932e8a0c6985f6d0af642f873
fia42192 <fia42192@spseol.cz>
2023-10-12 21:21:25 +0200

    zkouska

--- Prvni_hra_github.py
+++ Prvni_hra_github.py
@@ -1,3 +1,19 @@
 #git add . - stage souborů
 #git commit - připravit na odeslání
 #git push - pushnout na github
+
+print("Vítej v mojí únikové hře")
+#room1
+volba_cesty = str(input("Jsi v první místnosti, máš na výběr ze dvou cest, z pravé a levé, napiš, kterou si přeješ zvolit. (pravá/levá): "))
+
+#pravá cesta
+if volba_cesty == "pravá":
+    print("Vybral sis pravou cestu, ")
+
+#levá cesta
+elif volba_cesty == "levá":
+    print("Vybral sis levou cestu, ")
+
+#špatná volba    
+else:
+    print("Game over!")
