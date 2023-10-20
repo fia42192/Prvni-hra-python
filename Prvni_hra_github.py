cfde7b11d2bd6a962f34aaa7a84a204bfb3d1696
fia42192 <fia42192@spseol.cz>
2023-10-20 23:50:48 +0200

    test

--- Prvni_hra_github.py
+++ Prvni_hra_github.py
@@ -497,4 +497,4 @@ else:
 #Úniková hra formou kvízu (historie, všeobecné otázky), ve které je cílem dostat se k trezoru s penězi a následně uniknout pryč.
 #Ve hře jsou dva trezory, je jen na tobě, k jakému se vydáš. K oběma trezorům vede jiná cesta.
 #Pozorně si zapisuj všechny číselné kódy, budeš je potřebovat pro otevření trezoru, protože heslo od trezoru jsou všechny dosavadní kódy zapsány za sebou.
-#Jedná se o můj úplně první projekt.
+#Jedná se o můj úplně první projekt..
